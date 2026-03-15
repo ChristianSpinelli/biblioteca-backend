c82a23996b2f2a486189c8a975cebab22d72e746
c.spin <c.spinelli93@gmail.com>
2026-03-15 17:18:52 -0300

    criação de entidade para resposta generica por mensagem.

---
+++ src/main/java/com/fiap/biblioteca_backend/models/Response.java
@@ -0,0 +1,13 @@
+package com.fiap.biblioteca_backend.models;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+    private String message;
+}
